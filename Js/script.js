77358819cae237d4be77c4f93d0760abe7ddeb1e
Heber <hebercristianoferreira@gmail.com>
2022-05-27 19:12:05 -0300

    Continuando Desenvolvimento

--- Js/script.js
+++ Js/script.js
@@ -2,28 +2,25 @@
 
 
 
+//const tecnologias = [
+    //"java","react","node", "python","php"
+//];
 
+//const palavrasScretas =  tecnologias[Math.floor(Math.random() * tecnologias.length)];
 
+//document.addEventListener("keydown",(evento) =>{
+    //const codigo = evento.keyCode; // 65 - 90 (intervalo)
+   // if(isLetra(codigo)) {
+     //   const letra = evento.key;
+      //  if( letraErradas.includes(letra)){
+            //mostraAvisoLetraRepetida();
+       // } else {
+           // if(palavraSecreta)
+       // }
+  //  }
 
-const tecnologias = [
-    "java","react","node", "python","php"
-];
+//})
 
-const palavrasScretas =  tecnologias[Math.floor(Math.random() * tecnologias.length)];
-
-document.addEventListener("keydown",(evento) =>{
-    const codigo = evento.keyCode; // 65 - 90 (intervalo)
-    if(isLetra(codigo)) {
-        const letra = evento.key;
-        if( letraErradas.includes(letra)){
-            mostraAvisoLetraRepetida();
-        } else {
-            if(palavraSecreta)
-        }
-    }
-
-})
-
-function isLetra(codigo){
-    return codigo >= 65 && codigo <= 90;
-}
+//function isLetra(codigo){
+   // return codigo >= 65 && codigo <= 90;
+//}
